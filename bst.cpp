c8dc4ba3561bc865bd5d0d46c13b94294a03f2fb
olivia-bernard <bernar_o1@denison.edu>
2023-10-20 11:11:16 -0400

    taking some notes about what we need to include

---
+++ bst.cpp
@@ -0,0 +1,10 @@
+// void bst.empty()
+// void insert(d, k)
+// T get(k) - gets the DATA associated with k
+// void remove(k)
+// T max_data()
+// int max_key()
+// T min_data()
+// int min_key()
+// string in_order()
+// void trim(low, high)
